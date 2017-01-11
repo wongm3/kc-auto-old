62ae6355b4be379c4007844a4e569d54611f908a
Matt Wong <matt.wong.rpi@gmail.com>
2017-01-11 10:53:32 -0500

    setup styles gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,80 @@
  */
 
 // General
+import del from 'del';
 import gulp from 'gulp';
 import gutil from 'gulp-util';
+import rename from 'gulp-rename';
+
+// Styles
+import minify from 'gulp-cssnano';
+import prefix from 'gulp-autoprefixer';
+import sass from 'gulp-sass';
 
 // Webpack
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import webpackConfig from './webpack.config';
 
+/**
+ * Paths to project folders
+ */
 
-gulp.task('webpack:build', (callback) => {
+let paths = {
+    output: 'dist/**/*',
+    styles: {
+        bootstrap: 'node_modules/bootstrap-sass/assets/stylesheets',
+        fontawesome: 'node_modules/font-awesome/scss',
+        input: 'src/css/app.scss',
+        output: 'dist/css'
+    },
+    fonts: {
+        bootstrap: 'node_modules/bootstrap-sass/assets/fonts/**/*',
+        fontawesome: 'node_modules/font-awesome/fonts/**/*',
+        output: 'dist/fonts/'
+    }
+};
+
+/**
+ * Gulp Tasks
+ */
+
+// Process, and minify Sass files
+gulp.task('build:styles', ['build:fonts'], () => gulp.src(paths.styles.input)
+    .pipe(sass({
+        includePaths: [
+            paths.styles.bootstrap,
+            paths.styles.fontawesome
+        ]
+    }))
+    .pipe(prefix({
+        browsers: ['last 2 version', '> 1%'],
+        cascade: true,
+        remove: true
+    }))
+    .pipe(gulp.dest(paths.styles.output))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minify({
+        discardComments: {
+            removeAll: true
+        }
+    }))
+    .pipe(gulp.dest(paths.styles.output))
+);
+
+gulp.task('build:fonts', ['build:fonts:bootstrap', 'build:fonts:fontawesome']);
+
+// Bootstrap fonts
+gulp.task('build:fonts:bootstrap', () => gulp.src(paths.fonts.bootstrap)
+    .pipe(gulp.dest(paths.fonts.output))
+);
+
+// Font Awesome fonts
+gulp.task('build:fonts:fontawesome', () => gulp.src(paths.fonts.fontawesome)
+    .pipe(gulp.dest(paths.fonts.output))
+);
+
+gulp.task('build:webpack', (callback) => {
     webpack(webpackConfig, (err, stats) => {
         if (err) {
             throw new gutil.PluginError('webpack:build', err);
@@ -22,6 +86,13 @@ gulp.task('webpack:build', (callback) => {
         }));
         callback();
     });
+});
+
+gulp.task('clean:dist', () => {
+    del.sync([
+        paths.output,
+        '!dist/index.html'
+    ])
 });
 
 gulp.task('webpack-dev-server', () => {
@@ -43,6 +114,14 @@ gulp.task('webpack-dev-server', () => {
  */
 
 // Production build
-gulp.task('build', ['webpack:build']);
+gulp.task('build', [
+    'clean:dist',
+    'build:styles',
+    'build:webpack'
+]);
 
-gulp.task('default', ['webpack-dev-server']);
+gulp.task('default', [
+    'clean:dist',
+    'build:styles',
+    'webpack-dev-server'
+]);
