69d8d0e9a3bc46ea12aba8aeef647fb835247d33
Matt Wong <matt.wong.rpi@gmail.com>
2017-01-11 21:59:17 -0500

    initialize sidebar

--- src/js/component/Main.jsx
+++ src/js/component/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './layout/Header';
+import Sidebar from './layout/Sidebar';
 
 const Main = () => {
     return (
         <span>
             <Header/>
+            <Sidebar/>
         </span>
     )
 };
---
+++ src/js/component/layout/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => (
+    <nav id="sidebar">
+        
+    </nav>
+);
+
+export default Sidebar;
