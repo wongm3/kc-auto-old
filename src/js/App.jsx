4a5fd7b76a8af290b7125bc5f28f3b166a825aa2
Matt Wong <matt.wong.rpi@gmail.com>
2017-01-23 20:59:49 -0500

    fix eslint errors

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import Main from './component/Main';
 
 ReactDOM.render(
-    <Main/>,
-    document.getElementById('app')
-);
+  <Main />,
+  document.getElementById('app'));
--- src/js/component/navigation/navbar/NavbarHeader.jsx
+++ src/js/component/navigation/navbar/NavbarHeader.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
-import {elementType} from 'react-prop-types';
+import { Navbar } from 'react-bootstrap';
+import { elementType } from 'react-prop-types';
 
 const propTypes = {
-    componentClass: elementType
+  componentClass: elementType,
 };
 
 const defaultProps = {
-    componentClass: Navbar.Header
+  componentClass: Navbar.Header,
 };
 
-const NavbarHeader = ({componentClass: Component, ...props}) => (
-    <Component
-        {...props} />
+const NavbarHeader = ({ componentClass: Component, ...props }) => (
+  <Component {...props} />
 );
 
 NavbarHeader.propTypes = propTypes;
